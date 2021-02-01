f0ae119c9474a783e5945f43fcddb178557a5b88
KVN2099 <kevinromerooviedo@gmail.com>
2021-02-01 12:59:43 -0600

    Created login page and basic functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import React from 'react';
+import Login from './Views/login/login';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const { auth } = useSelector(state => state);
   return (
     <>
+      {
+      !auth.loggedIn ? <Login /> : 
+      <></>
+      }
     </>
   );
 }
